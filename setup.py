2512124ee92bbc6c0e9a3e17bf59f88c1604f9c9
Sébastien DA ROCHA <sebastien@da-rocha.net>
2018-10-24 17:09:53 +0200

    Made package pip installable

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+import setuptools
+
+
+setuptools.setup(
+    name="mviewerstudio-cas",
+    version="0.1.0",
+    author="Sébastien DA ROCHA",
+    author_email="sebastien@da-rocha.net",
+    description="A CAS enabled Proxy for mviewerstudio",
+    packages=setuptools.find_packages(where="mviewerstudio_cas/*"),
+    classifiers=(
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        "Flask>=1.0.0,<1.1.0",
+        "Flask-CAS>=1.0.0,<1.1.0",
+    ],
+)
+
