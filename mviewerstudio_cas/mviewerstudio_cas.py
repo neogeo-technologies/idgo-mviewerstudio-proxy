221c25a18d4bf9933d4efb9226e16abb037d2ee9
bchartier <bchartier@neogeo.fr>
2019-02-18 09:12:05 +0100

    Revamp user_info response

--- mviewerstudio_cas/mviewerstudio_cas.py
+++ mviewerstudio_cas/mviewerstudio_cas.py
@@ -32,8 +32,8 @@ def privileged_user_required(func):
     @wraps(func)
     def decorated_view(*args, **kwargs):
         # Pré-traitement
-        _user = User.objects.get(username=cas.username, is_active=True).profile
-        if not (_user.is_admin or _user.is_referent or _user.is_contributor):
+        user = User.objects.get(username=cas.username, is_active=True).profile
+        if not (user.is_admin or user.is_referent or user.is_contributor):
             abort(403)
         return func(*args, **kwargs)
         # Post-traitement
@@ -98,7 +98,7 @@ def viewerstudio_delete_user_content():
         with open(filename, encoding="utf-8") as f:
             xml = xmltodict.parse(f.read(), process_namespaces=False)
 
-        logging.info(xml["config"]["metadata"]["rdf:RDF"])
+        # logging.info(xml["config"]["metadata"]["rdf:RDF"])
         description = xml["config"]["metadata"]["rdf:RDF"]["rdf:Description"]
         if description["dc:creator"] == cas.username:
             counter += 1
@@ -115,95 +115,124 @@ def viewerstudio_delete_user_content():
 @app.route("/viewerstudio/user_info")
 @login_required
 @privileged_user_required
-def viewerstudio_est():
-    user_dj = User.objects.get(username=cas.username, is_active=True)
+def viewerstudio_user_info():
+    user = User.objects.get(username=cas.username, is_active=True)
+    data = {}
 
     try:
-        data = serializer(user_dj)
+        data["userName"] = user.username
+        data["firstName"] = user.first_name
+        data["lastName"] = user.last_name
+        data["userGroups"] = []
+
+        organisations = set()
+
+        for organisation in user.profile.referent_for:
+            org_name = organisation.ckan_slug
+            if org_name not in organisations:
+                organisations.add(org_name)
+                data["userGroups"].append(
+                    {
+                        "fullName": organisation.name,
+                        "slugName": organisation.ckan_slug,
+                        "userRole": "referent",
+                        "groupType": "organisation"
+                    }
+                )
+
+        for organisation in user.profile.contribute_for:
+            org_name = organisation.ckan_slug
+            if org_name not in organisations:
+                organisations.add(org_name)
+                data["userGroups"].append(
+                    {
+                        "fullName": organisation.name,
+                        "slugName": organisation.ckan_slug,
+                        "userRole": "contributor",
+                        "groupType": "organisation"
+                    }
+                )
+
+        logging.info(data)
+
     except (FieldError, ValueError) as e:
         logging.error(e)
         return flask.abort(400)
     else:
-        if len(data) == 0:
+        if not data:
             raise flask.abort(404)
         else:
             return jsonify(data)
+
+
+def get_user_content_in_folder(folder, user_role):
+    conf = get_conf()
+    user_content = []
+
+    # List regular XML files
+    entries = (os.path.join(folder, fn) for fn in os.listdir(folder))
+    entries = ((os.stat(path), path) for path in entries)
+    entries = (
+        path
+        for stat, path in entries
+        if S_ISREG(stat[ST_MODE]) and path.endswith(".xml")
+    )
+
+    for filename in entries:
+        with open(filename, encoding="utf-8") as f:
+            xml = xmltodict.parse(f.read(), process_namespaces=False)
+
+            # admin user can access any xml file
+            # referent user can access any xml file for all organisation he is a referent for
+            # contributor user can access xml files of which he is the creator
+            description = xml["config"]["metadata"]["rdf:RDF"]["rdf:Description"]
+            if not (user_role == "contributor" and description["dc:creator"] == cas.username):
+                url = filename.replace(
+                    conf["export_conf_folder"], conf["conf_path_from_mviewer"]
+                )
+                metadata = {
+                    "url": url,
+                    "creator": description["dc:creator"],
+                    "date": description.get("dc:date", ""),
+                    "title": description.get("dc:title", ""),
+                    "subjects": description.get("dc:subject", ""),
+                    "group": os.path.basename(folder),
+                }
+                user_content.append(metadata)
+
+    return user_content
 
 
 @app.route("/viewerstudio/srv/list.php")
 @login_required
 @privileged_user_required
 def viewerstudio_list_user_content():
-    conf = get_conf()
-    _user = User.objects.get(username=cas.username, is_active=True)
+    user = User.objects.get(username=cas.username, is_active=True)
 
-    folders = []
-    data = []
+    folders = set()
+    user_content = []
 
-    if _user.profile.is_admin:
-        folders.extend(
-            [
-                os.path.join(conf["export_conf_folder"], fn)
-                for fn in os.listdir(conf["export_conf_folder"])
-                if os.path.isdir(os.path.join(conf["export_conf_folder"], fn))
-            ]
-        )
-    else:
-        if _user.profile.organisation:
-            folders.append(
-                os.path.join(
-                    conf["export_conf_folder"], _user.profile.organisation.ckan_slug
-                )
-            )
+    for organisation in user.profile.referent_for:
+        folder = os.path.join(conf["export_conf_folder"], organisation.ckan_slug)
+        if folder not in folders:
+            folders.add(folder)
+            user_content.extend(get_user_content_in_folder(folder=folder, user_role="referent"))
 
-        for organisation in _user.profile.referent_for:
-            folder = os.path.join(conf["export_conf_folder"], organisation.ckan_slug)
-            if not folders or folder not in folders:
-                folders.append(folder)
+    for organisation in user.profile.contribute_for:
+        folder = os.path.join(conf["export_conf_folder"], organisation.ckan_slug)
+        if folder not in folders:
+            folders.add(folder)
+            user_content.extend(get_user_content_in_folder(folder=folder, user_role="contributor"))
 
-    if not folders:
-        return jsonify(data)
-
-    for folder in folders:
-
-        # sort files by age
-        entries = (os.path.join(folder, fn) for fn in os.listdir(folder))
-        entries = ((os.stat(path), path) for path in entries)
-        entries = (
-            (stat[ST_MTIME], path)
-            for stat, path in entries
-            if S_ISREG(stat[ST_MODE]) and path.endswith(".xml")
-        )
-
-        for _, filename in sorted(entries):
-            with open(filename, encoding="utf-8") as f:
-                xml = xmltodict.parse(f.read(), process_namespaces=False)
-
-                # logging.error(xml["config"]["metadata"]["rdf:RDF"])
-                description = xml["config"]["metadata"]["rdf:RDF"]["rdf:Description"]
-                if description["dc:creator"] == cas.username or _user.profile.is_admin:
-                    url = filename.replace(
-                        conf["export_conf_folder"], conf["conf_path_from_mviewer"]
-                    )
-                    metadata = {
-                        "url": url,
-                        "creator": description["dc:creator"],
-                        "date": description.get("dc:date", ""),
-                        "title": description.get("dc:title", ""),
-                        "subjects": description.get("dc:subject", ""),
-                        "group": os.path.basename(folder),
-                    }
-                    data.append(metadata)
-
-    return jsonify(data)
+    return jsonify(user_content)
 
 
 @app.route("/viewerstudio/srv/store.php", methods=["POST"])
 @login_required
 @privileged_user_required
 def viewerstudio_store_user_content():
-
-    _user = User.objects.get(username=cas.username, is_active=True)
+    # TODO: add a parameter for folder
+    user = User.objects.get(username=cas.username, is_active=True)
 
     conf = get_conf()
     xml0 = flask.request.data
@@ -219,7 +248,7 @@ def viewerstudio_store_user_content():
 
     # Create organization repo if not exists
     _map_directory = os.path.join(
-        conf["export_conf_folder"], _user.profile.organisation.ckan_slug
+        conf["export_conf_folder"], user.profile.organisation.ckan_slug
     )
     if not os.path.exists(_map_directory):
         os.makedirs(_map_directory)
@@ -227,7 +256,7 @@ def viewerstudio_store_user_content():
     filename = "{filename}.xml".format(filename=_map_title)
 
     file_path = os.path.join(_map_directory, filename)
-    relative_file_path = "{}/{}".format(_user.profile.organisation.ckan_slug, filename)
+    relative_file_path = "{}/{}".format(user.profile.organisation.ckan_slug, filename)
 
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(xml)
@@ -236,7 +265,7 @@ def viewerstudio_store_user_content():
         {
             "success": True,
             "filepath": relative_file_path,
-            "organisation": _user.profile.organisation.ckan_slug,
+            "group": user.profile.organisation.ckan_slug,
         }
     )
 
@@ -264,4 +293,3 @@ application = app
 if __name__ == "__main__":
     app.debug = True
     app.run()
-
